9232d05214ad5a1391e43e3159eb6932f3cb5bbe
Andrey Batutin <abatutin@dataart.com>
2017-07-12 11:39:49 +0300

    init commit

---
+++ ml_io_test/ml_io_test/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  ml_io_test
+//
+//  Created by Andrey Batutin on 7/12/17.
+//  Copyright © 2017 Andrey Batutin. All rights reserved.
+//
+
+import UIKit
+import CoreML
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
