a89750441bbf00039370d9528e172e79430c371c
Andrey Batutin <abatutin@dataart.com>
2017-07-13 12:05:40 +0300

    added test model and some images

--- ml_io_test/ml_io_test/ViewController.swift
+++ ml_io_test/ml_io_test/ViewController.swift
@@ -8,19 +8,41 @@
 
 import UIKit
 import CoreML
+import Vision
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let img = UIImage(named:"see.jpg")!
+        detectScene(image: CIImage(image: img)!)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func detectScene(image: CIImage) {
+        guard let model = try? VNCoreMLModel(for: GoogLeNetPlaces().model) else {
+            fatalError("we're fucked")
+        }
+        
+        let request = VNCoreMLRequest(model: model){ [weak self] request, error in
+            guard let res = request.results as? [VNClassificationObservation],
+                let topResult = res.first else{
+                    fatalError("we're fucked")
+            }
+            
+            print(topResult.identifier)
+            print(topResult.confidence)
+        }
+        
+        let handler = VNImageRequestHandler(ciImage: image)
+        DispatchQueue.global(qos: .userInteractive).async {
+            do{
+               try handler.perform([request])
+            }catch{
+                print(error)
+            }
+        }
     }
-
-
+    
 }
 
